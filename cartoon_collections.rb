18bd5728c50791712f034902cacdad1a58ffd295
Mike C <mikecronin92@gmail.com>
2018-03-23 04:42:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,13 @@ def roll_call_dwarves(dwarves)
 end
 
 
-def summon_captain_planet(array)
-  return array.map {|phrase| phrase.capitalize << '!'}
+def summon_captain_planet(summons)
+  return summons.map {|phrase| phrase.capitalize << '!'}
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  return calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
